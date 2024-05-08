9385c0dd23278dea6cb5f3e5c54ddc15ef3eefaf
cornelius31415 <50702246+cornelius31415@users.noreply.github.com>
2024-05-08 16:45:43 +0200

    Add files via upload

---
+++ Stellar Classification/StellarClassification.py
@@ -0,0 +1,168 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed May  8 15:45:50 2024
+
+@author: cornelius
+"""
+
+# ----------------------------------------------------------------------------
+
+# -----------------       STELLAR CLASSIFICATION         ---------------------
+
+# ----------------------------------------------------------------------------
+
+print("----------------------------------------------------------------------------")
+print()
+print(" -----------------       STELLAR CLASSIFICATION         ---------------------")
+print()
+print("----------------------------------------------------------------------------")
+
+# ---------------------------- IMPORTS ---------------------------------
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.neighbors import KNeighborsClassifier
+
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn import metrics
+from sklearn.metrics import confusion_matrix
+import matplotlib.pyplot as plt
+
+
+
+# ------------------------- DATA PREPARATION --------------------------
+
+df = pd.read_csv("star_classification.csv")
+df.drop(columns='obj_ID')
+df.dropna()
+print(df.info())
+
+features = df.drop(columns=["class"])
+labels   = df['class']
+
+feature_train, feature_test, label_train, label_test = train_test_split(
+    features,labels, test_size=0.2, random_state=42)
+
+# ----------------------------------------------------------------------------
+#                             DECISION TREE   
+# ----------------------------------------------------------------------------
+
+
+decision_tree = DecisionTreeClassifier()
+decision_tree.fit(feature_train,label_train)
+prediction = decision_tree.predict(feature_test)
+
+
+conf_matrix = confusion_matrix(label_test, prediction)
+cm_display = metrics.ConfusionMatrixDisplay(confusion_matrix = conf_matrix, 
+                                            display_labels = [0,1,2])
+cm_display.plot()
+plt.show()
+
+
+accuracy = metrics.accuracy_score(label_test, prediction)
+precision = metrics.precision_score(label_test, prediction,average='weighted')
+sensitivity = metrics.recall_score(label_test, prediction,average='weighted')
+f1_score = metrics.f1_score(label_test, prediction,average='weighted')
+
+print()
+print("----------------------- Decision Tree Results -----------------------")
+print()
+
+print("The accuracy is: ", accuracy)
+print("The precision is: ", precision)
+print("The sensitivity is: ", sensitivity)
+print("The F1 Score is: ", f1_score)
+
+"""
+                            OUTPUT
+                    
+               The accuracy is:  0.96465
+               The precision is:  0.9647081456158808
+               The sensitivity is:  0.96465
+               The F1 Score is:  0.9646778479183328 
+"""
+
+
+# ----------------------------------------------------------------------------
+#                             K NEAREST NEIGHBORS   
+# ----------------------------------------------------------------------------
+# pip install threadpoolctl==3.1.0
+
+knn = KNeighborsClassifier()
+knn.fit(feature_train, label_train)
+
+prediction = knn.predict(feature_test)
+
+
+
+conf_matrix = confusion_matrix(label_test, prediction)
+cm_display = metrics.ConfusionMatrixDisplay(confusion_matrix = conf_matrix, 
+                                            display_labels = [0,1,2])
+cm_display.plot()
+plt.show()
+
+
+accuracy = metrics.accuracy_score(label_test, prediction)
+precision = metrics.precision_score(label_test, prediction,average='weighted')
+sensitivity = metrics.recall_score(label_test, prediction,average='weighted')
+f1_score = metrics.f1_score(label_test, prediction,average='weighted')
+
+print()
+print("----------------------- KNN Results -----------------------")
+print()
+
+print("The accuracy is: ", accuracy)
+print("The precision is: ", precision)
+print("The sensitivity is: ", sensitivity)
+print("The F1 Score is: ", f1_score)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
